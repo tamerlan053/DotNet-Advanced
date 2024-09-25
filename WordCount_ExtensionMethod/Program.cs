a7384c17cf9e6220df07187fc62f30b528eb764f
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-25 21:35:50 +0200

    Add files via upload

---
+++ WordCount_ExtensionMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace WordCount;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please provide a sentence:");
+        string sentence = Console.ReadLine();
+        int wordCount = sentence.WordCount();
+
+        Console.WriteLine($"Word count: {wordCount}");
+
+        Console.WriteLine("Press any key to close...");
+        Console.ReadKey(true);
+    }
+}
---
+++ WordCount_ExtensionMethod/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace WordCount;
+
+public static class StringExtensions
+{
+    public static int WordCount(this string input)
+    {
+        return input.Split(new char[] { ' ', '.', ',', '?' },
+            StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+}
