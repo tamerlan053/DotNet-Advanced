87855ff22f131bd5a55ac88b187f772d2927f82d
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-30 22:57:16 +0200

    Add files via upload

---
+++ LinqExamples/LinqExamples/OrderByExamples.cs
@@ -0,0 +1,16 @@
+﻿using LinqExamples.Models;
+
+namespace LinqExamples;
+
+public class OrderByExamples
+{
+    public double[] SortAnglesFromBigToSmall(double[] angles)
+    {
+        throw new NotImplementedException("Use LINQ to implement this method");
+    }
+
+    public IList<Person> SortPersonsFromYoungToOldAndThenOnNameAlphabetically(List<Person> persons)
+    {
+        throw new NotImplementedException("Use LINQ to implement this method");
+    }
+}
