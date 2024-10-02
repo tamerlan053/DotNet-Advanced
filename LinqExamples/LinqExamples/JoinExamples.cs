f63b18263e8a9abeded84696adf2b3eb53e31028
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-02 11:15:58 +0200

    Update JoinExamples.cs
    both methods implented using LINQ

--- LinqExamples/LinqExamples/JoinExamples.cs
+++ LinqExamples/LinqExamples/JoinExamples.cs
@@ -6,11 +6,20 @@ public class JoinExamples
 {
     public int[] GetEvenNumbersOfIntersection(int[] firstList, int[] secondList)
     {
-        throw new NotImplementedException("Use LINQ to implement this method");
+        var evenNumbersQuery = from number1 in firstList
+                               join number2 in secondList on number1 equals number2
+                               where number1 % 2 == 0
+                               select number1;
+
+        return evenNumbersQuery.ToArray();
     }
 
     public IList<string> MatchPersonsOnBirthMonth(IList<Person> group1, IList<Person> group2)
     {
-        throw new NotImplementedException("Use LINQ to implement this method");
+        var matchPersonsQuery = from person1 in group1
+                                 join person2 in group2 on person1.BirthDate.Month equals person2.BirthDate.Month
+                                 select $"{person1.Name} and {person2.Name}";
+
+        return matchPersonsQuery.ToArray();
     }
 }
