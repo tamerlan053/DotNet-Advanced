4b1765b2b1d265d8ffcb4d4549af714693d7e587
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-10-14 22:14:49 +0200

    New online store project added

---
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,20 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public PieController(IPieRepository pieRepository)
+        {
+            this._pieRepository = pieRepository;
+        }
+        public IActionResult List()
+        {
+            //IEnumerable<Pie> pies = _pieRepository.GetAllPies(); 
+            return View();
+        }
+    }
+}
---
+++ BethanysPieShop/BethanysPieShop/Models/IPieRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace BethanysPieShop.Models;
+
+public interface IPieRepository
+{
+    Task<IEnumerable<Pie>> GetAllPies();
+    IEnumerable<Pie> GetPiesOfTheWeek();
+    Pie? GetById(int id);
+
+    IEnumerable<Pie> SearchPies(string searchQuery);
+}
