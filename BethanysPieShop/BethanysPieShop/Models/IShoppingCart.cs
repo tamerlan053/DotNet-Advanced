040212a03ad103f07e655249ea86e7be963aea09
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-17 18:52:36 +0200

    All IShoppingCart methods implemented

--- BethanysPieShop/BethanysPieShop/Models/IShoppingCart.cs
+++ BethanysPieShop/BethanysPieShop/Models/IShoppingCart.cs
@@ -5,4 +5,7 @@ public interface IShoppingCart
     void AddToCart(Pie pie);
     int RemoveFromCart(Pie pie);
     List<ShoppingCartItem> GetShoppingCartItems();
+    void ClearCart();
+    decimal GetShoppingCartTotal();
+   // List<ShoppingCartItem> ShoppingCartItems { get; set; }
 }
