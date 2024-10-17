f3486f8f3807c28d3514ffe0a24d58f860fbe797
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-17 18:52:04 +0200

    ShoppingCartItem list created

--- BethanysPieShop/BethanysPieShop/Models/IShoppingCart.cs
+++ BethanysPieShop/BethanysPieShop/Models/IShoppingCart.cs
@@ -1,1 +1,8 @@
+namespace BethanysPieShop.Models;
 
+public interface IShoppingCart
+{
+    void AddToCart(Pie pie);
+    int RemoveFromCart(Pie pie);
+    List<ShoppingCartItem> GetShoppingCartItems();
+}
