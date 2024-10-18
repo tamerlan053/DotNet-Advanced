3c485bb67edec6b2898819b9d9761c0c15098a04
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-18 23:32:03 +0200

    IShoppingCart interface implemented

--- BethanysPieShop/BethanysPieShop/Models/ShoppingCart.cs
+++ BethanysPieShop/BethanysPieShop/Models/ShoppingCart.cs
@@ -2,6 +2,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BethanysPieShop.Models;
 
-public class ShoppingCart {
-  
+public class ShoppingCart : IShoppingCart {
+    private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+
+    public string? ShoppingCartId { get; }
+
+    //public List<ShoppingCartItem> ShoppingCartItems { get; set; } = default!;
+
+    private ShoppingCart(BethanysPieShopDbContext bethanysPieShopDbContext, string cartId)
+    {
+        ShoppingCartId = cartId;
+        _bethanysPieShopDbContext = bethanysPieShopDbContext;
+    }
 }
