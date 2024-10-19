57ab49689fb391177e506213e849ed5ce220aa8f
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-10-19 22:26:57 +0200

    Pie claass with properties

--- BethanysPieShop/BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ BethanysPieShop/BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fbe456408d321dd2facaf908d952eaeeab219c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79e16ed7fe52e0125c9d5305f3c4774c7685e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
