a77c294ee5ba3f9369b3d070a48a9b67e25bbd39
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-10-30 23:25:57 +0100

    All properties implemented in ShoppingCartItem

--- BethanysPieShop/BethanysPieShop/Models/ShoppingCartItem.cs
+++ BethanysPieShop/BethanysPieShop/Models/ShoppingCartItem.cs
@@ -2,5 +2,9 @@
 {
     public class ShoppingCartItem
     {
+        public int ShoppintCartItemID { get; set; }
+        public Pie Pie { get; set; } = default!;
+        public int Amount { get; set; }
+        public string? ShoppingCartId { get; set; }
     }
 }
