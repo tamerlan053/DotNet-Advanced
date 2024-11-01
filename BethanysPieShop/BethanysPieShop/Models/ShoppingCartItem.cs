2098dec426e5dbcdf7cd63de7493219153bded08
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-11-01 23:01:19 +0100

    Update ShoppingCartItem.cs
    Fix typo ShoppingCartItemId

--- BethanysPieShop/BethanysPieShop/Models/ShoppingCartItem.cs
+++ BethanysPieShop/BethanysPieShop/Models/ShoppingCartItem.cs
@@ -5,7 +5,7 @@ namespace BethanysPieShop.Models
     public class ShoppingCartItem
     {
         [Key]
-        public int ShoppintCartItemID { get; set; }
+        public int ShoppingCartItemID { get; set; }
         public Pie Pie { get; set; } = default!;
         public int Amount { get; set; }
         public string? ShoppingCartId { get; set; }
