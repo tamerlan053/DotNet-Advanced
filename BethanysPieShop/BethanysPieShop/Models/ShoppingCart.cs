640763e3e20eb2cfc092f16b97457c2cc4371f7d
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-02 23:14:33 +0100

    IShoppingCart interface implemened in ShoppingCart

--- BethanysPieShop/BethanysPieShop/Models/ShoppingCart.cs
+++ BethanysPieShop/BethanysPieShop/Models/ShoppingCart.cs
@@ -1,6 +1,33 @@
-﻿namespace BethanysPieShop.Models
+﻿
+namespace BethanysPieShop.Models
 {
-    public class ShoppingCart
+    public class ShoppingCart : IShoppingCart
     {
+        public List<ShoppingCartItem> ShoppingCartItems { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void AddToCard(Pie pie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveFromCart(Pie pie)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
