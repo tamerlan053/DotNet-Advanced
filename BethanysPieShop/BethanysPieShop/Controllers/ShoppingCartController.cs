83f3fc1fcd1421b0a5577a711586820145bb30bc
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-04 20:49:29 +0100

    IPieRepository & IShoppingCart added to ShoppingCartController

--- BethanysPieShop/BethanysPieShop/Controllers/ShoppingCartController.cs
+++ BethanysPieShop/BethanysPieShop/Controllers/ShoppingCartController.cs
@@ -1,6 +1,18 @@
-﻿namespace BethanysPieShop.Controllers
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
 {
-    public class ShoppingCartController
+    public class ShoppingCartController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IPieRepository pieRepository, IShoppingCart shoppingCart)
+        {
+            _pieRepository = pieRepository;
+            _shoppingCart = shoppingCart;
+
+        }
     }
 }
