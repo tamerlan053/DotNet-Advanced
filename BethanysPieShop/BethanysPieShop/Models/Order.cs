7a9ecb9250793fe6e6f31050dfaff21cbcca995f
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-10 23:43:15 +0100

    firstname and lastname fields added to Order.cs

--- BethanysPieShop/BethanysPieShop/Models/Order.cs
+++ BethanysPieShop/BethanysPieShop/Models/Order.cs
@@ -7,5 +7,17 @@ namespace BethanysPieShop.Models
     {
         [BindNever]
         public int OrderId { get; set; }
+
+        public List<OrderDetail>? OrderDetails { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First name")]
+        [StringLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last name")]
+        [StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
     }
 }
