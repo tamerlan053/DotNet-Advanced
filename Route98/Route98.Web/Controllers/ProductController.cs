9ca3fdcf177da44d07f108f67c2d2b3bc62096ce
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-10 23:33:59 +0100

    New MVC project added

---
+++ Route98/Route98.Web/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Route98.Web.ViewModels;
+
+namespace Route98.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Detail(Guid id)
+        {
+            return View(id);
+        }
+
+        public IActionResult Overview(string categoryCode, int productsPerPage = 10)
+        {
+            return View(new ProductOverviewViewModel{ CategoryCode = categoryCode, ProductsPerPage = productsPerPage});
+        }
+    }
+}
---
+++ Route98/Route98.Web/ViewModels/ProductOverviewViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Route98.Web.ViewModels
+{
+    public class ProductOverviewViewModel
+    {
+        public string CategoryCode { get; set; } = string.Empty;
+        public int ProductsPerPage { get; set; }
+
+    }
+}
