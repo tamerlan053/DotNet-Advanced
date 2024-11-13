6f6f169084ad8b488e6b90c69d79335401c5abaf
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-13 21:04:49 +0100

    IOrderRepository fully implemented in OrderRepository

--- BethanysPieShop/BethanysPieShop/Models/OrderRepository.cs
+++ BethanysPieShop/BethanysPieShop/Models/OrderRepository.cs
@@ -9,6 +9,32 @@ namespace BethanysPieShop.Models
         {
             _bethanysPieShopDbContext = bethanysPieShopDbContext;
             _shoppingCart = shoppingCart;
-        }    
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+
+            List<ShoppingCartItem>? shoppingCartItems = _shoppingCart.ShoppingCartItems;
+            order.OrderTotal = _shoppingCart.GetShoppingCartTotal();
+
+            order.OrderDetails = new List<OrderDetail>();
+
+            foreach (ShoppingCartItem? shoppingCartItem in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    Amount = shoppingCartItem.Amount,
+                    PieId = shoppingCartItem.Pie.PieId,
+                    Price = shoppingCartItem.Pie.Price
+                };
+
+                order.OrderDetails.Add(orderDetail);
+            }
+
+            _bethanysPieShopDbContext.Orders.Add(order);
+
+            _bethanysPieShopDbContext.SaveChanges();
+        }
     }
 }
