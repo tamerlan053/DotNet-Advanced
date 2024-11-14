03e85c22e89f1f2efb7ba451953a1088bbc5723d
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-14 16:57:03 +0100

    IOrderRepository & IShoppingCart initialised in OrderController

--- BethanysPieShop/BethanysPieShop/Controllers/OrderController.cs
+++ BethanysPieShop/BethanysPieShop/Controllers/OrderController.cs
@@ -5,5 +5,13 @@ namespace BethanysPieShop.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, IShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
     }
 }
