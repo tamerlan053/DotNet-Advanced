b90eb8974c1ef1a4eeb2e3f137089d654a911811
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-19 00:50:01 +0100

    Scaffolding reverse engineering

---
+++ EfCore/ReverseEngineer/PubDatabaseContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReverseEngineerBefore;
+
+public partial class PubDatabaseContext : DbContext
+{
+    public PubDatabaseContext()
+    {
+    }
+
+    public PubDatabaseContext(DbContextOptions<PubDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasIndex(e => e.AuthorId, "IX_Books_AuthorId");
+
+            entity.Property(e => e.BasePrice).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books).HasForeignKey(d => d.AuthorId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
