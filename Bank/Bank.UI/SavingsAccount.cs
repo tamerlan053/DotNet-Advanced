277a8127ce18908d300a6e78cccb907b068f077b
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-21 09:28:04 +0100

    Bank NUnit project added

---
+++ Bank/Bank.UI/SavingsAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace Bank.UI
+{
+    public class SavingsAccount : BankAccount
+    {
+        public SavingsAccount(string owner, double interestRate, double startingBalance = 0) : base(owner, interestRate, startingBalance)
+        {
+        }
+
+        public override void Withdraw(double amount)
+        {
+            _balance -= amount;
+        }
+
+        public override void AddInterests()
+        {
+            _balance = _balance + _interestRate;
+        }
+
+        public override string ToString()
+        {
+            return "Savingsaccount as text";
+        }
+    }
+}
