2b0124e08374228b0e303ab47a507d9271b990d7
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-21 09:35:37 +0100

    NUnit class library - Bank

---
+++ Bank/Bank.UI.Tests/CheckingAccountTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Bank.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+
+namespace Bank.UI.Tests
+{
+    [TestFixture]
+    public class CheckingAccountTests
+    {
+        [Test]
+        public void Account_HasCorrectBalanceAfterConstruction()
+        {
+            var initialBalance = 2500;
+            var interestRate = 0.05;
+            var owner = "TestOwner";
+
+            var account = new CheckingAccount(owner, initialBalance, interestRate);
+
+            Assert.That(account.Balance, Is.EqualTo(initialBalance));
+        }
+
+        [Test]
+        public void Deposit_AddsAmountToTheBalance()
+        {
+            var account = new CheckingAccount("Tamerlan", 0.05, 2500);
+
+            account.Deposit(500);
+
+            Assert.That(account.Balance, Is.EqualTo(3000));
+        }
+
+        [Test]
+        public void Balance_IncreasesCorrectlyAfterYearlyInterests()
+        {
+            var account = new SavingsAccount("Tamerlan", 0.05, 2500);
+
+            account.AddInterests();
+
+            Assert.That(account.Balance, Is.EqualTo(2625));
+        }
+    }
+}
