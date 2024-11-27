3b3ce37bce623bf861e3adc2fa33fa8f57b1ce30
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-11-27 23:55:41 +0100

    SearchController method implemented

--- BethanysPieShop/BethanysPieShop/Controllers/SearchController.cs
+++ BethanysPieShop/BethanysPieShop/Controllers/SearchController.cs
@@ -1,1 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace BethanysPieShop.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
