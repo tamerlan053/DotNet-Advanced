07d3130bee0fbd254ee7e99a556322931d5cfb1e
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-12-01 12:49:15 +0100

    Contact controller implemented

---
+++ BethanysPieShop/BethanysPieShop/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
